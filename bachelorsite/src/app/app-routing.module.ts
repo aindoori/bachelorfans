730faaf9bd26296a3ab748a7d17b23e8f0bafab2
reneelinmitchell <reneelinmitchell@gmail.com>
2020-04-16 13:45:59 -0400

    modified some front end

--- bachelorsite/src/app/app-routing.module.ts
+++ bachelorsite/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
+const routes: Routes = [
+{
+    path: 'home',
+    component: HomeComponent
+  },
 
-const routes: Routes = [];
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
