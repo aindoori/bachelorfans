0603d436df44838d5d48b8845bb22751a497819f
reneelinmitchell <reneelinmitchell@gmail.com>
2020-04-18 17:14:37 -0400

    ugh can't use php

--- bachelorsite/src/app/app.module.ts
+++ bachelorsite/src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BachelorformComponent } from './bachelorform/bachelorform.component';
 import { ApplicantformComponent } from './applicantform/applicantform.component';
-import { LoginComponent } from './user/login/login.component';
-import { LogoutComponent } from './user/logout/logout.component';
-import { RegisterApplicantsComponent } from './user/register-applicants/register-applicants.component';
 import { ContestantsComponent } from './contestants/contestants.component';
 import { DatesComponent } from './dates/dates.component';
 import { ViewersComponent } from './viewers/viewers.component';
@@ -18,14 +13,10 @@ import { ViewersComponent } from './viewers/viewers.component';
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     HomeComponent,
     PageNotFoundComponent,
     BachelorformComponent,
     ApplicantformComponent,
-    LoginComponent,
-    LogoutComponent,
-    RegisterApplicantsComponent,
     ContestantsComponent,
     DatesComponent,
     ViewersComponent
