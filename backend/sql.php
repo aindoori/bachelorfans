159bca387d360ba819640e27f7e4ccb506b7c614
reneelinmitchell <reneelinmitchell@gmail.com>
2020-04-20 17:39:20 -0400

    new sql file sorry

--- backend/sql.php
+++ backend/sql.php
@@ -1,90 +1,44 @@
 <?php
 
-function addUser($name, $pwd, $cid, $dept, $role, $email, $profile_pic) {
+function addApplicant($name, $pwd, $age, $city, $state, $profession, $reason) {
 	global $db;
-	$query = "insert into user (name, hash_pwd, compID, dept, role, email, imgSrc) values (:name, :pwd, :cid, :dept, :role, :email, :imgSrc)";
+	$query = "insert into applicants (name, hash_pwd, age, hometown_city, hometown_state, profession, reason) values (:name, :pwd, :age, :hometown_city, :hometown_state, :profession, :reason)";
 	$statement = $db->prepare($query);
 	$statement->bindValue(':name', $name);
 	$statement->bindValue(':pwd', $pwd);
-	$statement->bindValue(':cid', $cid);
-	$statement->bindValue(':dept', $dept);
-	$statement->bindValue(':role', $role);
-	$statement->bindValue(':email', $email);
-	$statement->bindValue(':imgSrc', $profile_pic);
+	$statement->bindValue(':age', $age);
+	$statement->bindValue(':hometown_city', $city);
+	$statement->bindValue(':hometown_state', $state);
+	$statement->bindValue(':profession', $profession);
+	$statement->bindValue(':reason', $reason);
 	$statement->execute();
 	$statement->closeCursor();
 }
 
-function updateUser($name, $pwd, $cid, $dept, $role, $email, $profile_pic) {
+function updateApplicant($name, $pwd, $age, $city, $state, $profession, $reason) {
 	global $db;
-	$query = "update user set name=:name, hash_pwd=:pwd, dept=:dept, role=:role, email=:email, imgSrc=:imgSrc where compID=:cid";
+	$query = "update applicants set hash_pwd=:pwd, age=:age, hometown_city=:hometown_city, hometown_state=:hometown_state, profession=:profession, reason=:reason where name=:name";
 	$statement = $db->prepare($query);
 	$statement->bindValue(':name', $name);
 	$statement->bindValue(':pwd', $pwd);
-	$statement->bindValue(':cid', $cid);
-	$statement->bindValue(':dept', $dept);
-	$statement->bindValue(':role', $role);
-	$statement->bindValue(':email', $email);
-	$statement->bindValue(':imgSrc', $profile_pic);
+	$statement->bindValue(':age', $age);
+	$statement->bindValue(':hometown_city', $city);
+	$statement->bindValue(':hometown_state', $state);
+	$statement->bindValue(':profession', $profession);
+	$statement->bindValue(':reason', $reason);
 	$statement->execute();
 	$statement->closeCursor();
 }
 
-function getUserByID($cid) {
+// have them type their username and password to confirm deletion
+function deleteApplicant($name) {
 	global $db;
-	$query = "select * from user where compID=:cid";
+	$query = "delete from applicants where name=:name";
 	$statement = $db->prepare($query);
-	$statement->bindValue(':cid', $cid);
-	$statement->execute();
-	$results = $statement->fetchAll();
-	$statement->closeCursor();
-	return $results;
-}
-
-function addProject($title, $semester, $year, $dept, $faculty, $abstract, $proposal, $imgSrc) {
-	global $db;
-	$query = "insert into projectpage (title, semester, year, department, facultyAdvisor, abstract, projectProposal, imgSrc) values (:title, :semester, :year, :dept, :faculty, :abstract, :proposal, :imgSrc)";
-	$statement = $db->prepare($query);
-	$statement->bindValue(':title', $title);
-	$statement->bindValue(':semester', $semester);
-	$statement->bindValue(':year', $year);
-	$statement->bindValue(':dept', $dept);
-	$statement->bindValue(':faculty', $faculty);
-	$statement->bindValue(':abstract', $abstract);
-	$statement->bindValue(':proposal', $proposal);
-	$statement->bindValue(':imgSrc', $imgSrc);
+	$statement->bindValue(':name', $name);
 	$statement->execute();
 	$statement->closeCursor();
 }
 
-function addTeamMember($project_title, $teamMember) {
-	global $db;
-	$query = "insert into teamMembers (project_title, teamMember) values (:project_title, :teamMember)";
-	$statement = $db->prepare($query);
-	$statement->bindValue(':project_title', $project_title);
-	$statement->bindValue(':teamMember', $teamMember);
-	$statement->execute();
-	$statement->closeCursor();
-}
-
-function addKeyword($project_title, $keyword) {
-	global $db;
-	$query = "insert into keywords (project_title, keyword) values (:project_title, :keyword)";
-	$statement = $db->prepare($query);
-	$statement->bindValue(':project_title', $project_title);
-	$statement->bindValue(':keyword', $keyword);
-	$statement->execute();
-	$statement->closeCursor();
-}
-
-function addSkillNeeded($project_title, $skill) {
-	global $db;
-	$query = "insert into skillsNeeded (project_title, skill) values (:project_title, :skill)";
-	$statement = $db->prepare($query);
-	$statement->bindValue(':project_title', $project_title);
-	$statement->bindValue(':skill', $skill);
-	$statement->execute();
-	$statement->closeCursor();
-}
 
 ?>
