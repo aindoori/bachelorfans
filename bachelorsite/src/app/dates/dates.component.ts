eb4fa951f64e0cb66d5331912e1e001b3b670a31
Akshitha Indoori <a.indoori@gmail.com>
2020-04-21 18:19:34 -0400

    added code for courted and dates

--- bachelorsite/src/app/dates/dates.component.ts
+++ bachelorsite/src/app/dates/dates.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+//import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-dates',
   templateUrl: './dates.component.html',
   styleUrls: ['./dates.component.css']
 })
-export class DatesComponent implements OnInit {
+export class DatesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  title = 'angular8phpmyadmindatabse';
+  data = [];
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost/dates.php').subscribe(data => {
+    this.data.push(data);
+    console.log(this.data);
+   
+    
+    }, error => console.error(error));
   }
 
 }
